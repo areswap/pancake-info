54ab89989a185f548802ad9559fc91ceb787ea1c
areswap <82188583+areswap@users.noreply.github.com>
2021-04-09 11:41:56 +0700

    Update GlobalPage.js

--- src/pages/GlobalPage.js
+++ src/pages/GlobalPage.js
@@ -65,11 +65,11 @@ function GlobalPage() {
 
   return (
     <PageWrapper>
-      <ThemedBackground backgroundColor={transparentize(0.8, '#4FD8DE')} />
+      <ThemedBackground backgroundColor={transparentize(0.8, '#FF009D')} />
       <ContentWrapper>
         <div>
           <AutoColumn gap="24px" style={{ paddingBottom: below800 ? '0' : '24px' }}>
-            <TYPE.largeHeader>{below800 ? 'Analytics' : 'PancakeSwap Analytics'}</TYPE.largeHeader>
+            <TYPE.largeHeader>{below800 ? 'Analytics' : 'Airswap Analytics'}</TYPE.largeHeader>
             <Search />
             <GlobalStats />
           </AutoColumn>
