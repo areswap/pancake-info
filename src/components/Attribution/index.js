ef818d91841daa5d12cc894d8c50dd489cf6f7fa
areswap <82188583+areswap@users.noreply.github.com>
2021-04-09 11:31:59 +0700

    Update index.js

--- src/components/Attribution/index.js
+++ src/components/Attribution/index.js
@@ -2,12 +2,12 @@ import React from 'react'
 
 const Attribution = () => (
   <p className="attribution">
-    <a href="https://github.com/pancakeswap/pancakeswap-info" rel="noopener noreferrer" target="_blank">
+    <a href="https://github.com/airswap" rel="noopener noreferrer" target="_blank">
       Github
     </a>{' '}
     |{' '}
-    <a href="https://pancakeswap.io" rel="noopener noreferrer" target="_blank">
-      pancakeswap
+    <a href="https://airswap.finance" rel="noopener noreferrer" target="_blank">
+      Airswap
     </a>{' '}
     |{' '}
     <a href="https://gifer.com/en/1Atv" rel="noopener noreferrer" target="_blank">
