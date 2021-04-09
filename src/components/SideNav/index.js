30ea8f5b222c0bc1a3aa911175936b5dea1fa2ed
areswap <82188583+areswap@users.noreply.github.com>
2021-04-09 11:47:32 +0700

    Update index.js

--- src/components/SideNav/index.js
+++ src/components/SideNav/index.js
@@ -163,17 +163,17 @@ function SideNav({ history }) {
           </AutoColumn>
           <AutoColumn gap="0.5rem" style={{ marginLeft: '.75rem', marginBottom: '4rem' }}>
             <HeaderText>
-              <Link href="https://pancakeswap.finance/" target="_blank">
-                PancakeSwap
+              <Link href="https://airswap.finance/" target="_blank">
+                Airswap
               </Link>
             </HeaderText>
             <HeaderText>
-              <Link href="https://docs.pancakeswap.finance/" target="_blank">
+              <Link href="https://docs.airswap.finance/" target="_blank">
                 Docs
               </Link>
             </HeaderText>
             <HeaderText>
-              <Link href="https://twitter.com/PancakeSwap " target="_blank">
+              <Link href="https://twitter.com/airswapprotocol " target="_blank">
                 Twitter
               </Link>
             </HeaderText>
